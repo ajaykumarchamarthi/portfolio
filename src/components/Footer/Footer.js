4772d19b12d0dd0ce173c36016a9e0aed37c28be
ajayaj0302 <ajayaj0302@gmail.com>
2022-04-28 19:37:11 +0530

    footer color fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Footer.module.css";
 
 function Footer() {
-  return <div className={classes.footer}>Made With by Ajay Kumar</div>;
+  return <div className={classes.footer}>Made by Ajay Kumar</div>;
 }
 
 export default Footer;
